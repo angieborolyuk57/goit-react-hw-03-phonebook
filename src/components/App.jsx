50031982b8427bfb7b9d87c11abd4b300f56aac3
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2024-01-05 23:03:33 +0100

    initial files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
+import React, { useState } from 'react';
+import ContactList from './ContactList';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+
+const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setFilter(value);
+  };
+
+  const handleAddContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  const handleDeleteContact = (id) => {
+    const updatedContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(updatedContacts);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ContactForm onAddContact={handleAddContact} contacts={contacts} />
+      <Filter filter={filter} onInputChange={handleInputChange} />
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        onDeleteContact={handleDeleteContact}
+      />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import css from './ContactList.module.css';
+
+const ContactForm = (props) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
+
+  const handleAddContact = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+
+    if (!name || !number) {
+      alert('Please enter both name and phone number');
+      return;
+    }
+
+    if (props.contacts.some((contact) => contact.name === name)) {
+      alert(`${name} already exists in contacts and cannot be added.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    props.onAddContact(newContact);
+
+    // Reset form state after adding contact
+    setName('');
+    setNumber('');
+    setSubmitted(false);
+  };
+
+  return (
+    <form className={css.form}>
+      <label className={css.label}>
+        Name
+        <input
+          id={nanoid()}
+          className={`${css.input} ${submitted && !name && css.error}`}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <label>
+        Phone number
+        <input
+          id={nanoid()}
+          className={`${css.input} ${submitted && !number && css.error}`}
+          type="number"
+          name="number"
+          value={number}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <button className={css.addBtn} onClick={handleAddContact}>
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
